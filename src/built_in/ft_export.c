7c48f6db276b0ac165fbcadbc43f96bbc8bb5923
Edson Firmino <efirmino@c1r6p1.42nice.fr>
2023-02-11 08:57:06 +0100

    got efirmino branch's modifs

--- src/built_in/ft_export.c
+++ src/built_in/ft_export.c
@@ -1,60 +1,60 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   export.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: efirmino <efirmino@student.42nice.fr>      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/09 11:01:22 by efirmino          #+#    #+#             */
-/*   Updated: 2023/02/09 14:10:03 by efirmino         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        :::      ::::::::   */
+// /*   ft_export.c                                        :+:      :+:    :+:   */
+// /*                                                    +:+ +:+         +:+     */
+// /*   By: efirmino <efirmino@student.42nice.fr>      +#+  +:+       +#+        */
+// /*                                                +#+#+#+#+#+   +#+           */
+// /*   Created: 2023/02/09 11:01:22 by efirmino          #+#    #+#             */
+// /*   Updated: 2023/02/11 08:40:50 by efirmino         ###   ########.fr       */
+// /*                                                                            */
+// /* ************************************************************************** */
 
-#include "../../include/minishell.h"
+// #include "../../include/minishell.h"
 
-static void	ft_check_n_add(char *str)
-{
-	char	*key;
-	char	*value;
-	int		i;
-	int		j;
-	char	**splitted;
+// static void	ft_check_n_add(char *str)
+// {
+// 	char	*key;
+// 	char	*value;
+// 	int		i;
+// 	// int		j;
+// 	char	**splitted;
 
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == '=')
-		{
-			value = ft_strdup(str + (i + 1));
-			splitted = ft_split(str, '=');
-			key = splitted[0];
-			if (ft_is_alphanum(value))
-			ft_add_element(key, value, data.minishell_env);
-		}
-		i++;
-	}
-	free(value);
-	ft_freesplit(splitted);
-}
+// 	i = 0;
+// 	while (str[i])
+// 	{
+// 		if (str[i] == '=')
+// 		{
+// 			value = ft_strdup(str + (i + 1));
+// 			splitted = ft_split(str, '=');
+// 			key = splitted[0];
+// 			// if (ft_is_alphanum(value))
+// 			ft_add_env_element(key, value);
+// 		}
+// 		i++;
+// 	}
+// 	free(value);
+// 	ft_freesplit(splitted);
+// }
 
-void	ft_export(t_cmd *command)
-{
-	pid_t	child;
-	char	**cmd;
-	char	**to_export;
-	int		i;
-	int		j;
+// void	ft_export(t_cmd *command)
+// {
+// 	pid_t	child;
+// 	char	**cmd;
+// 	// char	**to_export;
+// 	int		i;
+// 	// int		j;
 
-	child = fork();
-	cmd = command->cmd;
-	if (child == 0)
-	{
-		i = 1;
-		while (cmd[i])
-		{
-			ft_check_n_add(cmd[i]);
-			i++;
-		}
-	}
-	waitpid(child, 0, 0);
-}
+// 	child = fork();
+// 	cmd = command->cmd;
+// 	if (child == 0)
+// 	{
+// 		i = 1;
+// 		while (cmd[i])
+// 		{
+// 			ft_check_n_add(cmd[i]);
+// 			i++;
+// 		}
+// 	}
+// 	waitpid(child, 0, 0);
+// }
