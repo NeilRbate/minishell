6f9e36dd918e15a16124e8666c153e982a377ec5
jbarbate <jbarbate@student.42nice.fr>
2023-02-27 14:14:38 +0100

    Corrected redir

--- src/parsing/redir.c
+++ src/parsing/redir.c
@@ -6,7 +6,7 @@
 /*   By: jbarbate <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 09:19:57 by jbarbate          #+#    #+#             */
-/*   Updated: 2023/02/27 13:29:42 by jbarbate         ###   ########.fr       */
+/*   Updated: 2023/02/27 14:13:58 by jbarbate         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,37 @@
 
 int	ft_lastredir(t_id *id, int type)
 {
-	int	fd;
+	int		fd;
+	t_id	*stock;
 
-	fd = ft_openredir (id->data, type, id);
-	id->type = 20;
+	fd = ft_openredir(id->data, type, id);
 	if (fd < 0)
-		return (-1);
+		return (ft_putendl_fd("error: fail to open", 2), -1);
+	id->type = 20;
 	while (id->next != NULL && id->type != 3)
 	{
 		id = id->next;
-		if (id->type >= 7 && id->type <= 10)
+		if (id->type == 0)
+			stock = id;
+		else if (id->type >= 7 && id->type < 10)
 		{
 			close(fd);
 			type = id->type;
 			fd = -1;
-			while (id->next != NULL && id->type != 0)
+			while (id->next != NULL && id->type != 0 && id->type != 3)
 			{
 				id = id->next;
-				if (id->type != 0 && id->type != 5 && id->type != 1 &&
-					id->type != 2)
+				if (id->type != 5 && id->type != 0)
 					return (ft_putendl_fd("error: invalid syntax", 2), -1);
 				if (id->type == 0)
 				{
 					fd = ft_openredir(id->data, type, id);
 					id->type = 20;
 					if (id->next != NULL)
+					{
 						id = id->next;
-					ft_del_idelem(id->prev);
+						ft_del_idelem(id->prev);
+					}
 					break;
 				}
 			}
@@ -53,18 +57,14 @@ int	ft_redir(t_id *id)
 {
 	int		fd;
 	int		type;
-	t_id	*stock;
 
 	type = id->type;
-	stock = id;
-	while (id != NULL && id->type != 0)
+	while (id->next != NULL && id->type != 0)
 	{
 		id = id->next;
 		if (id->type != 5 && id->type != 0)
 			return (ft_putendl_fd("error: invalid syntax", 2), -1);
 	}
-	if (id == NULL)
-		return (ft_putendl_fd("error: invalid syntax", 2), -1);
 	fd = ft_lastredir(id, type);
 	return (fd);
 }
@@ -73,8 +73,10 @@ int	ft_redirctrl(t_id *id)
 {
 	t_id	*stock;
 	int		fd;
+	int		index;
 
 	stock = id;
+	index = id->index;
 	ft_print_lex(id);
 	while (id->type != 0)
 		id = id->next;
