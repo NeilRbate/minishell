9b4427340b3fb548d6789c140bb04940f6e58487
Edson Firmino <efirmino@c2r1p1.42nice.fr>
2023-03-16 15:32:33 +0100

    freed all global variables

--- src/built_in/ft_exit.c
+++ src/built_in/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: efirmino <efirmino@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 09:16:17 by efirmino          #+#    #+#             */
-/*   Updated: 2023/03/16 10:29:32 by efirmino         ###   ########.fr       */
+/*   Updated: 2023/03/16 15:25:18 by efirmino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,9 +50,15 @@ void	ft_exit(t_cmd *command)
 
 	ft_putendl_fd("exit", 1);
 	if (!command->cmd[1])
+	{
+		ft_free_global();
 		exit (1);
+	}
 	exit_code = -1;
 	ft_get_exit_code(command->cmd, &exit_code);
 	if (exit_code != -1)
+	{
+		ft_free_global();
 		exit(exit_code);
+	}
 }
