ffeabc88c0747513d6f28e35bd96340b2381c314
jbarbate <jbarbate@student.42nice.fr>
2023-03-29 13:48:16 +0200

    Nothing to say

--- src/parsing/exportquote.c
+++ src/parsing/exportquote.c
@@ -6,7 +6,7 @@
 /*   By: jbarbate <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 15:25:47 by jbarbate          #+#    #+#             */
-/*   Updated: 2023/03/29 13:42:03 by jbarbate         ###   ########.fr       */
+/*   Updated: 2023/03/29 13:48:04 by jbarbate         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,13 +32,15 @@ void	ft_exportquote(t_id *id)
 					return ;
 				if (id->type == 0)
 					stock = id;
+				else
+					return ;
 				if (id->next)
 					id = id->next;
 				if (id->type == 5)
 					return ;
 				while (id->next && id->type != 0)
 					id = id->next;
-				if (id->prev && (id->prev->type == 2 || id->prev->type == 1) && stock != NULL)
+				if (id->prev && (id->prev->type == 2 || id->prev->type == 1))
 					if (id->next && (id->next->type == 2 || id->next->type == 1))
 					{
 						stock->data = ft_strjoin(stock->data, id->data);
